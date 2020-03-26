049cbbab442724a84aceaa4de144d83403f40166
Eric Gregory <egregory3@my.apsu.edu>
2020-03-25 22:07:06 -0500

    Added Get Quantities, loop logic to display all items added.
    ###TO DO ###
    JSON request for each item, add EditText for quantity and TV for measurement

--- app/src/main/java/com/esquared/nutricalc/EnterItems.java
+++ app/src/main/java/com/esquared/nutricalc/EnterItems.java
@@ -7,41 +7,51 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class EnterItems extends AppCompatActivity {
-int nextItemID = 1;
+int nextItemID = 0;
 LinearLayout rl;
 Button saveBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_enter_items);
         rl = findViewById(R.id.EnterItems);
-
+        //function to create row
         createRow(rl);
-
+        //bring in buttons from layout
         Button addRowBtn = findViewById(R.id.btnAddRow);
         saveBtn = findViewById(R.id.btnSaveIngredients);
-
+        //on click listener for AddRowbutton
        addRowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 createRow(rl);
             }
         });
-
+        //listener fo save button
        saveBtn.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
+               //create an array list of strings and populate items into it.
+               ArrayList<String> items = new ArrayList<>();
+               for(int i=0; i<nextItemID; i++){
+                   EditText current = findViewById(i);
+                   items.add(current.getText().toString());
+               }
+
+               //package intent and start activity
                Intent intent = new Intent(getApplicationContext(), GetQuantities.class);
                intent.putExtra("Rows", nextItemID);
+               intent.putStringArrayListExtra("Items", items);
                startActivity(intent);
            }
        });
@@ -49,16 +59,19 @@ Button saveBtn;
     }
 
     protected void createRow(LinearLayout rl){
+        //increment rows, create EditText and add to layout
 
-        EditText et = new EditText(this);
+        EditText et  = new EditText(this);
         et.setId(nextItemID);
         et.setHeight(150);
         et.setWidth(350);
         et.setTextSize(25);
-        et.setText("Test " + nextItemID);
+        et.setText("");
+        et.setHint("Enter next Item");
 
         rl.addView(et);
         nextItemID ++;
+
 
 
     }
--- app/src/main/java/com/esquared/nutricalc/GetQuantities.java
+++ app/src/main/java/com/esquared/nutricalc/GetQuantities.java
@@ -2,21 +2,43 @@ package com.esquared.nutricalc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class GetQuantities extends AppCompatActivity {
-TextView testTV;
-Intent intent = getIntent();
-Integer rows;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        intent.getIntExtra("Rows", 0);
         setContentView(R.layout.activity_get_quantities);
-        testTV = findViewById(R.id.testTV);
-        testTV.setText(rows.toString());
+
+        //Get Intent and Declare Variables to hold incoming intents
+        Intent intent = getIntent();
+
+        LinearLayout myItems = findViewById(R.id.ll_itemsDisplay);
+
+        //get data from Intent
+        ArrayList<String> items = intent.getStringArrayListExtra("Items");
+
+        //Create a row for each item in Items and add to TextView
+        for(int i=0; i < items.size(); i++){
+
+            TextView tv = new TextView(this);
+            tv.setText(items.get(i));
+            tv.setHeight(150);
+            tv.setWidth(350);
+            tv.setTextSize(25);
+
+            myItems.addView(tv);
+
+        }
+
 
     }
 }
